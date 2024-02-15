5aec2ffda990d16e8ed0f2a11364f5c4ac6673ce
Mad37b <Miguelkit37@hotmail.com>
2024-02-15 14:35:37 +0100

    Primer Commit , Crear JFrame y metodo cliente existe

---
+++ src/RegexCliente.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+
+public class RegexCliente {
+	
+	// Instancia de la clase RegexCliente 
+	
+	RegexCliente Validar = new RegexCliente();
+	
+	Pattern PatternNif = Pattern.compile(null);
+	Pattern PattenrCorreo = Pattern.compile(null); 
+	Pattern PatternTelefono = Pattern.compile (null);
+	
+ public RegexCliente() {
+	// TODO Auto-generated constructor stub
+}
+}
