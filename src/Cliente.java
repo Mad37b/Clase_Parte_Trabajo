26d3dc6064b99a94f3bbd753f89f8dae08c18e04
Mad37b <Miguelkit37@hotmail.com>
2024-02-19 13:56:36 +0100

    Primer Commit con JFrame sin acabar

--- src/Cliente.java
+++ src/Cliente.java
@@ -19,11 +19,23 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
-public class Cliente extends JFrame implements Serializable  {
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Window.Type;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Cliente extends JFrame implements Serializable {
 
 	/** JavaDoc **/
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	/**
 	 * @author ULISES III
 	 * 
@@ -43,15 +55,12 @@ public class Cliente extends JFrame implements Serializable  {
 	 */
 
 	// Atributos de la clase
-	
-	private JPanel panel ;
+
+	private JPanel panel;
 	private JTextField textNombre;
 	private JTextField textNif;
 	private JTextField textCorreo;
 	private JTextField textTelefono;
-	
-	
-	private JButton buttonAlta;
 
 	// Lista Array
 	static List<Cliente> listaClientes = new ArrayList<>();
@@ -71,17 +80,88 @@ public class Cliente extends JFrame implements Serializable  {
 	private Pattern telefonoPattern = Pattern.compile("^\\d{3}\\s\\d{3}\\s\\d{3}$");
 
 	// fichero
-	private String ficheroCliente;
+	// private String ficheroCliente;
 
 	public Cliente() {
 //  Nunca llamar a los metodos de la clase por evitar un bucle infinito
 		// verificarCliente();
-		//insertarDatos();
-		
+		// insertarDatos();
+
 		setTitle("Alta de Clientes");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(400, 500);
+		setSize(400, 300);
 		getContentPane().setLayout(null);
+
+		// JPanel ///////////////////////////////////////////
+
+		panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(panel);
+		panel.setLayout(null);
+
+		// JLabel Nombre , Nif, Correo, Telefono //////////////////
+
+		JLabel labelNombre = new JLabel("Nombre");
+		labelNombre.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelNombre.setBounds(43, 40, 72, 14);
+		getContentPane().add(labelNombre);
+
+		JLabel labelNif = new JLabel("Nif");
+		labelNif.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelNif.setBounds(45, 78, 46, 14);
+		getContentPane().add(labelNif);
+
+		JLabel labelCorreo = new JLabel("Correo");
+		labelCorreo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelCorreo.setBounds(43, 129, 61, 14);
+		getContentPane().add(labelCorreo);
+
+		JLabel labelTelefono = new JLabel("Telefono");
+		labelTelefono.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		labelTelefono.setBounds(43, 177, 72, 14);
+		getContentPane().add(labelTelefono);
+
+		// Texto Nombre , Nif , Correo , Telefono
+
+		textNombre = new JTextField();
+		textNombre.setBounds(204, 39, 120, 20);
+		getContentPane().add(textNombre);
+		textNombre.setColumns(10);
+
+		textNif = new JTextField();
+		textNif.setColumns(10);
+		textNif.setBounds(204, 77, 120, 20);
+		getContentPane().add(textNif);
+
+		textCorreo = new JTextField();
+		textCorreo.setColumns(10);
+		textCorreo.setBounds(204, 128, 120, 20);
+		getContentPane().add(textCorreo);
+
+		textTelefono = new JTextField();
+		textTelefono.setColumns(10);
+		textTelefono.setBounds(204, 176, 120, 20);
+		getContentPane().add(textTelefono);
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		JButton botonGenerarFichero = new JButton("Generar Fichero");
+		botonGenerarFichero.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				Cliente nuevoCliente = new Cliente();
+				ClienteExiste(nuevoCliente, listaClientes);
+				FicheroCliente fichero = new FicheroCliente();
+				nuevoCliente.insertarDatos();
+				nuevoCliente.archivoCliente(listaClientes);
+				fichero.crearFichero(nuevoCliente);
+				System.out.println(nuevoCliente);
+
+			}
+		});
+		botonGenerarFichero.setBounds(130, 215, 120, 35);
+		getContentPane().add(botonGenerarFichero);
 	};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -92,7 +172,7 @@ public class Cliente extends JFrame implements Serializable  {
 		System.out.println("Introduce los datos del cliente : Nombre, Nif, Correo, Telefono");
 		Cliente cliente = new Cliente();
 		while (!salir) {
-			
+
 			System.out.println("Nombre : ");
 
 			nombre = teclado.next();
@@ -120,14 +200,14 @@ public class Cliente extends JFrame implements Serializable  {
 			telefono = teclado.next();
 
 			Matcher matcherTelefono = telefonoPattern.matcher(telefono);
-			
+
 			if (matcherTelefono.find()) {
 				System.out.println("Nif valido en el  index: " + matcherTelefono.start());
 			} else {
 				System.out.println("Telefono no válido");
 				return;
 			}
-			
+
 			listaClientes.add(cliente);
 
 			// metodo para seguir o no introduciendo datos
@@ -212,28 +292,29 @@ public class Cliente extends JFrame implements Serializable  {
 		return " Cliente{" + "nombre='" + nombre + '\'' + ", nif='" + nif + '\'' + ", correo='" + correo + '\''
 				+ ", telefono='" + telefono + '\'' + '}';
 	}
-	
+
 	// validar Cliente
-	
-	// boolean 
-	private boolean existe;
-	public static  boolean ClienteExiste(Cliente nuevoCliente,List<Cliente> listaClientes ) {
-		
-		if ( !Cliente.existe(nuevoCliente, listaClientes) ) {
-			
-			verMensaje(" El cliente no se ha dado de alta"); 
+
+	// boolean
+	// private boolean existe;
+
+	public static boolean ClienteExiste(Cliente nuevoCliente, List<Cliente> listaClientes) {
+
+		if (!Cliente.existe(nuevoCliente, listaClientes)) {
+
+			verMensaje(" El cliente no se ha dado de alta");
 			return false;
-	}else {
-		
-		// por JoptionPanel
-		verMensaje("El cliente se ha dado de alta");
-		// por consola
-		return true;
+		} else {
+
+			// por JoptionPanel
+			verMensaje("El cliente se ha dado de alta");
+			// por consola
+			return true;
+		}
+
 	}
-		
-}
-	
-	private static boolean existe(Cliente nuevoCliente, List<Cliente> listaClientes2) {
+
+	private static boolean existe(Cliente nuevoCliente, List<Cliente> listaClientes) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -241,32 +322,41 @@ public class Cliente extends JFrame implements Serializable  {
 	private static void verMensaje(String mensaje) {
 		JOptionPane.showMessageDialog(null, mensaje);
 	}
-	
+
 	public static String pedirCliente() {
 		String pedirClientes = JOptionPane.showInputDialog("Introduce el cliente:");
-		verMensaje("Si el cliente existe paso al siguiente campo\n"+
-		           "si no existe hago que salte panel de alta cliente");
-		     
+		verMensaje(
+				"Si el cliente existe paso al siguiente campo\n" + "si no existe hago que salte panel de alta cliente");
+
 		return pedirClientes;
 	}
-	
-	// hacer Jframe de mi programa 
-	
-	// 
+
+	// hacer Jframe de mi programa
+
+	//
 
 	public static void main(String[] arg) {
-		Cliente nuevoCliente = new Cliente();
-		ClienteExiste(nuevoCliente, listaClientes);
-		FicheroCliente fichero = new FicheroCliente();
-		nuevoCliente.insertarDatos();
-		nuevoCliente.archivoCliente(listaClientes);
-		fichero.crearFichero(nuevoCliente);
-		System.out.println(nuevoCliente);
-		
-		
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cliente frame = new Cliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Pasarlo a Mouseclick action
+
+		/**
+		 * Cliente nuevoCliente = new Cliente(); ClienteExiste(nuevoCliente,
+		 * listaClientes); FicheroCliente fichero = new FicheroCliente();
+		 * nuevoCliente.insertarDatos(); nuevoCliente.archivoCliente(listaClientes);
+		 * fichero.crearFichero(nuevoCliente); System.out.println(nuevoCliente);
+		 **/
 
 		// Crear instancia de la clase para crear el fichero del cliente
 	}
-
-
 }
